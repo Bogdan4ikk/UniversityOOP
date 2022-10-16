7bf48e6538bb0fc82d37199faddc2bfa06c5ae01
Bogdan4ikk <bogdan.ivni@gmail.com>
2022-10-16 14:46:11 +0300

    OOP

---
+++ lab0A/Header.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include <Windows.h> // Бібліотека, яка дає змогу програмі працювати на російській мові
+#include <iomanip> // Бібліотека, за допомогою якої працює setw
+
+using namespace std; // Для того щоб, ми могли виводити дані на екран
+
+void square(int a = 2, int b = 2, int c = 2); // прототип функції, з умовно заданними параметрами
+
---
+++ lab0A/Source.cpp
@@ -0,0 +1,9 @@
+#include "Header.h" // підключення файлу заголовків
+
+void square(int a, int b, int c) {
+	cout << "\n\n";
+	cout << "Площа прямокутника = " << a * b;
+	cout << "\n";
+	cout << "Площа квадрата = " << c * c;
+	cout << "\n\n";
+}
